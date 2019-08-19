5b6c82388e1b4596facff2337870878707b801cb
skyler dowdy <skylerwebdev@gmail.com>
2019-08-19 17:27:14 -0400

    control testing

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import {render, fireEvent} from 'react-testing-library'
+import 'react-testing-library/cleanup-after-each'
 import Controls from './Controls'
-import { render, fireEvent } from "@testing-library/react";
 
-describe('<Controls />', () => {
-  it('matches snapshot', () => {
-    const tree = renderer.create(<Controls />);
-    expect(tree.toJSON()).toMatchSnapshot();
-  });
-
-});
+describe('<Controls />', () =>{
+  it('open and locked work as expected', () => {
+    const closeFake = jest.fn();
+    const {queryByText} = render(<Controls locked={false} closed={false} toggleClosed={closeFake} />);
+    const lockButton = queryByText('Lock Gate')
+    const closeButton = queryByText('Close Gate')
+    expect(lockButton)
+    expect(closeButton)
+    expect(lockButton.disabled).toBe(true);
+    expect(closeButton.disabled).toBe(false);
+    fireEvent.click(closeButton)
+    expect(closeFake).toBeCalled();
+  })
+  it('closed and unlocked work as expected', () => {
+    
+  })
+})
