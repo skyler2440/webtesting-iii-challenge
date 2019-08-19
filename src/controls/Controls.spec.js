1901d15c816691e5b83f91dbd20073cff29f22f0
skyler dowdy <skylerwebdev@gmail.com>
2019-08-19 15:28:26 -0400

    Snapshot Tests Written

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,14 @@
-// Test away!
+import React from 'react';
+import renderer from 'react-test-renderer'; // 1: install this npm module as a dev dependency
+import Controls from './Controls'
+
+describe('<Controls />', () => {
+  // 2. write this test
+  it('matches snapshot', () => {
+    const tree = renderer.create(<Controls />); // generates a DOM tree
+
+    // snapshots are a JSON representation of the DOM tree
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,14 @@
-// Test away
+import React from 'react';
+import renderer from 'react-test-renderer'; // 1: install this npm module as a dev dependency
+import Dashboard from './Dashboard'
+
+describe('<Dashboard />', () => {
+  // 2. write this test
+  it('matches snapshot', () => {
+    const tree = renderer.create(<Dashboard />); // generates a DOM tree
+
+    // snapshots are a JSON representation of the DOM tree
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+
+});
